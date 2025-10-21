0f271edeb5fd31ba2acf6c7b0362404006c73475
sayam09das <dassayam2021@gmail.com>
2025-10-21 12:06:33 +0530

    Refactor server setup: move Express and CORS middleware to app.js and clean up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Test route
-app.get('/', (req, res) => {
-    res.send("Backend server is running!");
-});
 
 // Start server
 app.listen(PORT, () => {
