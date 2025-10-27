8b3f80a388b148a2e4e534658ba40b2af854ae50
sayam09das <dassayam2021@gmail.com>
2025-10-27 11:20:24 +0530

    "Added useEffect hook to Login.jsx"

--- client/src/components/Auth/Loginpage/Login.jsx
+++ client/src/components/Auth/Loginpage/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { Eye, EyeOff, Mail, Lock, LogIn } from 'lucide-react';
